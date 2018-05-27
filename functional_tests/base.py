3329f2989d662afc14864428ab06849c646845cc
Olivier Desmaison <krossov@protonmail.com>
2018-05-27 16:29:30 +0200

    Use chrome driver to speed-up the test

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -14,7 +14,8 @@ class FunctionalTest(StaticLiveServerTestCase):
 
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        #self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
             self.live_server_url = 'http://' + staging_server
